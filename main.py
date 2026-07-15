9fc4345c69cc47080fbf7fc983621109d1616ae7
Caiã Ramos <caiaramos77@gmail.com>
2026-07-15 14:37:00 -0300

    Add initial book data structure

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
 print("Library Management System")
+
+books = []
+book = {
+        "title": "Harry Potter",
+        "author": "J.K. Rowling",
+        "year": 1997,
+        "isbn": "978..."
+    }
+books.append(book)
+book = {
+        "title": "The Hobbit",
+        "author": "J.R.R Tolkien",
+        "year": 1937,
+        "isbn": "978..."
+    }
+books.append(book)
+
+for book in books:
+    print(book["title"])
