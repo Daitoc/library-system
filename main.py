7b725ca4f4bef4bf7e2dde034f4f44eb81d47f09
Caiã Ramos <caiaramos77@gmail.com>
2026-07-16 16:52:03 -0300

    Refactor code using functions

--- main.py
+++ main.py
@@ -1,6 +1,39 @@
+def add_book(books):
+# Adds books to the library
+
+    title = input("Title: ")
+    author = input("Author: ")
+    year = int(input("Year: "))
+    isbn = input("ISBN: ")
+
+    book = {
+        "title": title,
+        "author": author,
+        "year": year,
+        "isbn": isbn
+    }
+    books.append(book)
+
+    print("\nBook added successfully!\n")
+
+def list_books(books):
+# Shows the books that are in the library
+
+    if len(books) == 0:
+        print("\nNo books found\n")
+    else:
+        print("\n===== Books =====\n")
+        for book in books:
+            print("Title:", book["title"])
+            print("Author:", book["author"])
+            print("Year:", book["year"])
+            print("ISBN:", book["isbn"])
+            print("\n----------------------\n")
+
 print("===== Library Management System =====")
 
 books = []
+
 
 while True:
 
@@ -15,36 +48,12 @@ while True:
 # Add a Book in Library
 
     if option == "1":
-        title = input("Title: ")
-        author = input("Author: ")
-        year = int(input("Year: "))
-        isbn = input("ISBN: ")
-
-        book = {
-            "title": title,
-            "author": author,
-            "year": year,
-            "isbn": isbn
-        }
-
-        books.append(book)
-
-        print("\nBook added successfully!\n")
+        add_book(books)
 
 # Shows the books in library
 
     elif option == "2":
-        if len(books) == 0:
-            print("\nno books found\n")
-        else:
-            print("\n===== Books =====\n")
-            for book in books:
-                print("Title:", book["title"])
-                print("Author:", book["author"])
-                print("Year:", book["year"])
-                print("ISBN:", book["isbn"])
-                print("\n----------------------\n")
-
+        list_books(books)
         
     elif option == "3":
         print("Bye!")
