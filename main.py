215963936a9bde94ae926a9ccc89028ca2be76b6
Caiã Ramos <caiaramos77@gmail.com>
2026-07-17 18:18:23 -0300

    Add search and remove book features

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def add_book(books):
-# Adds books to the library
+# Add a book to the library
 
     title = input("Title: ")
     author = input("Author: ")
@@ -31,20 +31,42 @@ def list_books(books):
             print("\n----------------------\n")
 
 def search_book(books):
-# Search for the book that is on the list
+# Search for a book in the library
     
     search = input("Enter the title: ")
     found = False
 
     for book in books:
         if search == book["title"]:
-            print("This is the book you are looking for:", book["title"])
             found = True
+            print("\n===== Book Found =====\n")
+            print("Book found: ")
+            print("Title:", book["title"])
+            print("Author:", book["author"])
+            print("Year:", book["year"])
+            print("ISBN:", book["isbn"])
+            print("\n---------------------------------------")
             break
         
-    if found == False:
+    if not found:
         print("\nBook not found.\n")
+
+def remove_book(books):
+# Remove the book that is on the list
+
+    search = input("Enter the title of the book you want to remove: ")
+    found = False
+
+    for book in books:
+        if search == book["title"]:
+            found = True
+            books.remove(book)
+            print("Book removed successfully!")
+            break
         
+    if not found:
+        print("\nBook not found.\n")
+    
         
 
 
@@ -61,23 +83,32 @@ while True:
     print("1 - Add Book")
     print("2 - List Books")
     print("3 - Search Books")
-    print("4 - Exit")
+    print("4 - Remove Book")
+    print("5 - Exit")
 
     option = input("Choose an option: ")
 
 # Add a Book in Library
-
     if option == "1":
         add_book(books)
 
 # Shows the books in library
-
     elif option == "2":
         list_books(books)
-    elif option == "3":
+
+# Search a book in library
+    elif option == "3":    
         search_book(books)
+    
+# Remove a book in library
     elif option == "4":
+        remove_book(books)
+
+# Exit the program
+    elif option == "5":
         print("Bye!")
         break
+
+# Invalid option
     else:
         print("\nInvalid option.\n")
